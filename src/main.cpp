de8d806b43aa679b8924b042a6d1cf5fcb72148d
Mike van Es <mikevanes97@gmail.com>
2021-05-16 15:34:55 +0200

    Proof of concept

--- src/main.cpp
+++ src/main.cpp
@@ -1,89 +1,36 @@
 #include <Arduino.h>
 #include <Drivers/MotorDriver.h>
 #include <Defines/MotorDefines.h>
-#include "Defines/PIDDefines.h"
 
 #include <Drivers/AcceleroDriver.h>
-#include <Controller/PIDcontroller.h>
 
 MotorDriver* md;
 AcceleroDriver* ad;
-PIDcontroller* pidC;
-float elapsedTime, time, timePrev;
-bool forward = false;
-bool bootUp = true;
 
 void setup() {
     // write your initialization code here
     Serial.begin(115200);
     md = new MotorDriver();
     ad = new AcceleroDriver();
-    pidC = new PIDcontroller();
 
     if(!ad->SetupWire()){
-        //Serial.println("Program crashed whilst initialising Accelero driver");
+        Serial.println("Program crashed whilst initialising Accelero driver");
     }
-
-    time = micros();
-}
-
-void turnMotor(){
-    // write your code here
-    md->TurnLeftMotor();
-//    md->TurnRightMotor();
-}
-
-void SetStep(double pid){
-    pid = abs(pid);
-    if(pid < 300){
-        md->SetStep(Steps::SIXTEENTH);
-    }
-
-    else if(pid < 800){
-        md->SetStep(Steps::QUARTER);
-    }
-
-
-//    else if(pid > 700){
-//        md->SetStep(Steps::HALF);
-//    }
 }
 
 void loop() {
-    time = micros();
-    elapsedTime = (time - timePrev)/1000;
     int angle = ad->GetAngle();
 
-    double pid = pidC->GetPID(angle, elapsedTime);
-
-    SetStep(pid);
-
-    if(abs(angle) > 0) {
-        if ((angle >= 0 && !forward) || (angle >= 0 && bootUp)) {
-            md->SetDirection(RIGHT_MOTOR_DIR, TURN_LEFT);
-            md->SetDirection(LEFT_MOTOR_DIR, TURN_LEFT);
-
-            forward = true;
-            bootUp = false;
-        } else if ((angle < 0 && forward) || (angle < 1 && bootUp)) {
-            md->SetDirection(RIGHT_MOTOR_DIR, TURN_RIGHT);
-            md->SetDirection(LEFT_MOTOR_DIR, TURN_RIGHT);
-
-            forward = false;
-            bootUp = false;
-        }
-
-        pid = abs(pid);
-
-        pid = MAX_SPEED - pid;
-        pid = pid <= MIN_SPEED ? MIN_SPEED : pid;
-
-        md->SetLeftMotorSpeed(pid);
-
-        turnMotor();
+    if (angle > 0) {
+        md->SetDirection(RIGHT_MOTOR_DIR, TURN_LEFT);
+        md->SetDirection(LEFT_MOTOR_DIR, TURN_LEFT);
+    } else if (angle < 0) {
+        md->SetDirection(RIGHT_MOTOR_DIR, TURN_RIGHT);
+        md->SetDirection(LEFT_MOTOR_DIR, TURN_RIGHT);
     }
 
-    timePrev = time;
+    md->TurnLeftMotor();
+    md->TurnRightMotor();
 }
 
 
